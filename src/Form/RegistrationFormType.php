f1a17c262bd92d2f59c7e71457093fd108d44111
slyriyad <156778836+slyriyad@users.noreply.github.com>
2024-09-19 06:41:30 +0200

    font des formulaires

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,16 +2,18 @@
 namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Vich\UploaderBundle\Form\Type\VichImageType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
 class RegistrationFormType extends AbstractType
 {
 public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -27,19 +29,28 @@ new IsTrue([
 ]),
 ],
 ])
-->add('plainPassword', PasswordType::class, [
-'mapped' => false,
-'attr' => ['autocomplete' => 'new-password'],
-'constraints' => [
-new NotBlank([
-'message' => 'Please enter a password',
-]),
-new Length([
-'min' => 6,
-'minMessage' => 'Your password should be at least {{ limit }} characters',
-'max' => 4096,
-]),
-],
+->add('plainPassword', RepeatedType::class, [
+    'type' => PasswordType::class,
+    'first_options' => [
+        'attr' => ['autocomplete' => 'new-password'],
+        'constraints' => [
+            new NotBlank([
+                'message' => 'Please enter a password',
+            ]),
+            new Length([
+                'min' => 6,
+                'minMessage' => 'Your password should be at least {{ limit }} characters',
+                'max' => 4096,
+            ]),
+        ],
+        'label' => 'Password',
+    ],
+    'second_options' => [
+        'attr' => ['autocomplete' => 'new-password'],
+        'label' => 'Confirm Password',
+    ],
+    'invalid_message' => 'The password fields must match.',
+    'mapped' => false, // Ne pas mapper à l'entité car il n'y a pas de champ correspondant
 ])
 ->add('imageFile', VichImageType::class, [
 'required' => false,
